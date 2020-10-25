c6ec8b421eb9c9b8f458a1a4ab0803a56f5cd430
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-10-25 13:25:12 +0100

    fix: SongComponent working

--- src/components/Songs/SongCard.jsx
+++ src/components/Songs/SongCard.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import './SongCard.scss'
 
 const SongCard = ({ song, typeSearch, form, handleOpen, addFav }) => {
     return (
-        <li>
-            <p>{song.name}</p>
-            <p>{song.artists[0].name}</p>
-            <p>{song.decade}</p>
-            <audio controls>
-                <source src={typeSearch ? song.preview_url : song.url} type='audio/mpeg' />
-            </audio>
-            {typeSearch ?
-                <div>
-                    <button onClick={() => handleOpen(song.name)}>Add Fav</button>
-                    {form === song.name ? <div><button onClick={() => addFav(song, "60's")}>60</button><button onClick={() => addFav(song, "70's")}>70</button></div> : null}
+        <li className="SongCard">
+            <img
+                className="albumImage"
+                src={typeSearch ? song.album.images[0].url : song.image}
+                alt={song.name} />
+            <div className="songCardInfo">
+                <div className='songText'>
+                    <p className='songName'>"{song.name}"</p>
+                    <p className='songArtist'>{song.artists[0].name} | {song.decade}</p>
                 </div>
-            : null}
+                <audio controls>
+                    <source src={typeSearch ? song.preview_url : song.url} type='audio/mpeg' />
+                </audio>
+                {typeSearch ?
+                    <div>
+                        <button onClick={() => handleOpen(song.name)}>Add Fav</button>
+                        {form === song.name ? <div><button onClick={() => addFav(song, "60's")}>60</button><button onClick={() => addFav(song, "70's")}>70</button></div> : null}
+                    </div>
+                    : null}
+            </div>
         </li>
     )
 }
