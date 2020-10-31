a3bbb6a2ec8794f97ad03c51625111250870e903
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-11-01 00:19:28 +0100

    feat: working on styles

--- src/components/Contacts/ContactModal.jsx
+++ src/components/Contacts/ContactModal.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import CloseModalButton from '../Generic/CloseModalButton'
-import InputText from '../Generic/InputText'
 import GenericButton from '../Generic/GenericButton'
 import { makeStyles } from '@material-ui/core/styles'
 import './ContactModal.scss'
@@ -18,9 +17,9 @@ const ContactModal = ({ closeModal, tempState, handleChange, handleFileUpload, h
     const classes = useStyles()
 
     const [edit, setEdit] = useState(false)
-    
+
     useEffect(() => {
-        if(tempState.id) {
+        if (tempState.id) {
             setEdit(true)
         }
     }, [])
@@ -32,124 +31,109 @@ const ContactModal = ({ closeModal, tempState, handleChange, handleFileUpload, h
             <h4 style={{ textAlign: 'center' }}>Completa los campos</h4>
 
             <form onSubmit={edit ? handleEditContact : modalSent} className={classes.root} noValidate autoComplete='off'>
-                <div className='LocationModalContainer'>
-                    <div>
-                        <br />
-                        <label>Nombre:</label>
-                        <InputText
-                            id='name'
-                            margin='normal'
-                            required
+                <div className='ContactModalContainer'>
+                <div className="form-row">
+                <div className='form-group col-md-8'>
+                        <label for="exampleFormControlInput1">Nombre</label>
+                        <input type="text"
+                            className="form-control"
+                            id="exampleFormControlInput1"
                             name='name'
                             value={tempState.name}
                             onChange={handleChange}
-                            variant='outlined' />
+                        />
                     </div>
-                    <div>
-                        <br />
-                        <label>Relación:</label>
-                        <InputText
-                            id='role'
-                            margin='normal'
-                            required
+                    <div className='form-group col-md-4'>
+                        <label for="exampleFormControlInput1">Relación</label>
+                        <input type="text"
+                            className="form-control"
+                            id="relación"
                             name='role'
                             value={tempState.role}
                             onChange={handleChange}
-                            variant='outlined'
-                            multiline />
+                        />
                     </div>
-                    <div>
-                        <br />
-                        <label>Dirección:</label>
-                        <InputText
-                            id='address'
-                            margin='normal'
-                            required
+                </div>
+                   
+                    <div className='form-group'>
+                        <label for="exampleFormControlInput1">Dirección</label>
+                        <input type="text"
+                            className="form-control"
+                            id="address"
                             name='address'
                             value={tempState.address}
                             onChange={handleChange}
-                            variant='outlined'
-                            multiline />
+                        />
                     </div>
-                    <div>
-                        <br />
-                        <label>Email:</label>
-                        <InputText
-                            id='email'
-                            margin='normal'
-                            required
+                    <div className="form-row">
+                    <div className='form-group col-md-4'>
+                        <label for="exampleFormControlInput1">Email</label>
+                        <input type="email"
+                            className="form-control"
+                            id="email"
                             name='email'
                             value={tempState.email}
                             onChange={handleChange}
-                            variant='outlined'
-                            multiline />
+                        />
                     </div>
-                    <div>
-                        <br />
-                        <label>Teléfono:</label>
-                        <InputText
-                            id='phone'
-                            margin='normal'
-                            required
+                    <div className='form-group col-md-4'>
+                        <label for="exampleFormControlInput1">Teléfono</label>
+                        <input type="text"
+                            className="form-control"
+                            id="phone"
                             name='phone'
                             value={tempState.phone}
                             onChange={handleChange}
-                            variant='outlined'
-                            multiline />
+                        />
                     </div>
-                    <div>
-                        <br />
-                        <label>Cumpleaños:</label>
-                        <InputText
-                            id='birthday'
-                            margin='normal'
-                            required
+                    <div className='form-group'>
+                        <label for="exampleFormControlInput1">Cumpleaños</label>
+                        <input type="text"
+                            className="form-control"
+                            id="birthday"
                             name='birthday'
                             value={tempState.birthday}
                             onChange={handleChange}
-                            variant='outlined'
-                            multiline />
-                    </div>
-                    <div>
-                        <br />
-                        <label>Descripción:</label>
-                        <InputText
-                            id='description'
-                            margin='normal'
-                            required
-                            name='description'
-                            value={tempState.description}
-                            onChange={handleChange}
-                            variant='outlined'
-                            multiline />
-                    </div>
-                    {!edit ? 
-                        <div>
-                            <br />
-                            <label>Foto:</label>
-                            <input
-                                type='file'
-                                name='url'
-                                onChange={(e) => handleFileUpload(e)}
-                                placeholder='Selecciona un archivo'
-                            />
-                        </div>
-                    : null}
-                    <div>
-                        <br />
-                        <GenericButton
-                            type='submit'
-                            fullWidth
-                            variant='contained'
-                            color='primary'
-                            className='mt-3'
-                            text='Guardar'
                         />
                     </div>
-                    <div>
-                        <img src={tempState.photo} style={{width: '200px'}} />
-                        {tempState.photo && <p>Imagen actual</p>}
                     </div>
+                    <div className='form-group'>
+                        <label for="description">Descripción</label>
+                        <textarea
+                            className="form-control"
+                            id="description"
+                            rows="3"
+                            name='description'
+                            value={tempState.description}
+                            onChange={handleChange}></textarea>
+                    </div>
+                    {!edit ?
+                        <div className="form-group">
+                        <label for="file">Foto</label>
+                        <input 
+                        type="file" 
+                        className="form-control-file" 
+                        id="foto"
+                        name='url'
+                        onChange={(e) => handleFileUpload(e)}
+                        placeholder='Selecciona un archivo'
+                        />
+                </div> : null}
+                    <div>
+                    <br />
+                    <GenericButton
+                        type='submit'
+                        fullWidth
+                        variant='contained'
+                        color='primary'
+                        className='mt-3'
+                        text='Guardar'
+                    />
+                </div>
+                <div>
+                    <img src={tempState.photo} style={{ width: '200px' }} />
+                    {tempState.photo && <p>Imagen actual</p>}
+                </div>
                 </div>
             </form>
         </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import ComponentHeader from '../Generic/ComponentHeader'
 import GenericButton from '../Generic/GenericButton'
 import { useAuthContext } from '../../contexts/AuthContext'
 import { login } from '../../services/Api'
@@ -40,50 +39,54 @@ const LogIn = () => {
 
   return (
     <div className='container'>
-      <div className='row'>
+      <div className='row NudoMap'>
         <div className='col-12 mt-5'>
-        <div className='NudoMap'>
-            <ComponentHeader
-              nudoIcon='https://res.cloudinary.com/difhe4gl3/image/upload/v1603296190/NUDO/assets/Dashboard-icons/Icon-login_cuaa4a.svg'
-              title='Entra'
-              description='Entra a NUDO con tu email y tu password.'
-            />
+          <div className='NudoMap'>
+            <h1 className='text-center'>Entrar</h1>
+            <p className='text-center'>Si ya tienes eres usuario de NUDO, ingresa tu correo y tu contraseña <br /> para acceder al menú principal.</p>
           </div>
         </div>
-        <div className='col-12 mt-5 LoginContainer'>
-        <div className='LoginImage'>
-          
-        </div>
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <div class='form-group'>
-              <label for='exampleInputEmail1'>Email address</label>
-              <input type='email'
-                class={`form-control ${state.error ? `is-invalid` : null}`}
-                id='email'
-                name='email'
-                aria-describedby='emailHelp'
-                onChange={handleChange} />
-              {state.error  ? 
-                <div class="invalid-feedback">
-                  Email o contraseña incorrecta
+        <div className='LogInContainer'>
+          <div className='LogInNudoIcon'>
+            <img src='https://res.cloudinary.com/difhe4gl3/image/upload/v1603296190/NUDO/assets/Dashboard-icons/Icon-login_cuaa4a.svg' />
+          </div>
+            <div className='LogInImage'>
+            </div>
+          <div className='LogInForm'>
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <div className='LogInFormContainer'>
+                <div class='form-group'>
+                  <label for='exampleInputEmail1'>Correo electrónico</label>
+                  <input type='email'
+                    class={`form-control ${state.error ? `is-invalid` : null}`}
+                    id='email'
+                    name='email'
+                    aria-describedby='emailHelp'
+                    onChange={handleChange} />
+                  {state.error ?
+                    <div class="invalid-feedback">
+                      Email o contraseña incorrecta
                 </div>
-                : null
-              }
-            </div>
-            <div class='form-group'>
-              <label for='exampleInputPassword1'>Password</label>
-              <input
-                type='password'
-                class={`form-control ${state.error ? `is-invalid` : null}`}
-                id='password'
-                name='password'
-                onChange={handleChange} />
-            </div>
-            <div class='form-group form-check'>
-              <label class='form-check-label' for='exampleCheck1'>Si no estas logeado <Link to='/signin'>Regístrate</Link></label>
-            </div>
-            <GenericButton text='Login' />
-          </form>
+                    : null
+                  }
+                </div>
+                <div class='form-group'>
+                  <label for='exampleInputPassword1'>Contraseña</label>
+                  <input
+                    type='password'
+                    class={`form-control ${state.error ? `is-invalid` : null}`}
+                    id='password'
+                    name='password'
+                    onChange={handleChange} />
+                </div>
+                <small class='form-text text-muted LogInText' for='exampleCheck1'>Si no tienes perfil, <Link to='/signin' style={{color: '#839672'}}>Regístrate</Link></small>
+                {/* <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> */}
+              </div>
+              <div className='LogInButton'>
+                <GenericButton text='Login' />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
