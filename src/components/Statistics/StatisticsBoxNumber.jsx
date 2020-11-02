f5225c67e02440a2e3326ea0897883d891c8f375
miguelbachi5a <miguelbachi5a@gmail.com>
2020-11-02 12:22:18 +0100

    feat: data in the top cards of statistics

--- src/components/Statistics/StatisticsBoxNumber.jsx
+++ src/components/Statistics/StatisticsBoxNumber.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './StatisticsBoxNumber.scss'
 
-const StatisticsBoxNumber = ({ selected, lastDays }) => {
+const StatisticsBoxNumber = ({ selected, lastDays, selectedInfo }) => {
+    const [totalLength, setTotalLength] = useState(0)
+
+    useEffect(() => {
+        if (selected === 'total') {
+            console.log(selectedInfo);
+            const allElementsLength = selectedInfo.images?.length + selectedInfo.songs?.length + selectedInfo.locations?.length + selectedInfo.contacts?.length + selectedInfo.gameScores?.length
+            setTotalLength(allElementsLength)
+        }
+    }, [selectedInfo])
+
     return (
         <div className='StatisticsBoxNumber'>
-            {lastDays ?
-                <><h5>Número de {selected.charAt(0).toUpperCase() + selected.slice(1)} en los últimos 7 días:</h5>
-                <h1 className='statisticsRightH'>9</h1></> :
-                <><h5>Número total de {selected.charAt(0).toUpperCase() + selected.slice(1)}:</h5>
-                <h1 className='statisticsRightH'>45</h1></>
+            {selected === 'total' ?
+                (lastDays ?
+                    <><h5>Total en los últimos 7 días:</h5>
+                    <h1 className='statisticsRightH'>Hacer</h1></> :
+                    <><h5>Total:</h5>
+                    <h1 className='statisticsRightH'>{totalLength}</h1></>
+                ) :
+                (lastDays ?
+                    <><h5>Número de {selected.charAt(0).toUpperCase() + selected.slice(1)} en los últimos 7 días:</h5>
+                    <h1 className='statisticsRightH'>{selectedInfo.length}</h1></> :
+                    <><h5>Número total de {selected.charAt(0).toUpperCase() + selected.slice(1)}:</h5>
+                    <h1 className='statisticsRightH'>{selectedInfo.length}</h1></>
+                )
             }
         </div>
     )
--- src/components/Statistics/StatisticsMenu.jsx
+++ src/components/Statistics/StatisticsMenu.jsx
@@ -1,15 +1,99 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import ComponentHeader from '../Generic/ComponentHeader'
 import StatisticsFavFilter from './StatisticsFavFilter'
 import StatisticsBoxNumber from './StatisticsBoxNumber'
+import { getSongs, getLocations, getImages, getContacts, getScores } from '../../services/Api'
 import './StatisticsMenu.scss'
 
 const StatisticsMenu = ({ setDecade }) => {
-    const [selected, setSelected] = useState('total')
+    const [total, setTotal] = useState({})
+    const [selected, setSelected] = useState()
     const [selectedInfo, setSelectedInfo] = useState([])
+
+    useEffect(() => {
+        getSongs()
+            .then(songs => {
+                setTotal(prev => {
+                    return {
+                        ...prev,
+                        songs: songs
+                    }
+                })
+            })
+            .then(() => {
+                getLocations()
+                    .then(locations => {
+                        setTotal(prev => {
+                            return {
+                                ...prev,
+                                locations: locations
+                            }
+                        })
+                    })
+            })
+            .then(() => {
+                getImages()
+                    .then(images => {
+                        setTotal(prev => {
+                            return {
+                                ...prev,
+                                images: images
+                            }
+                        })
+                    })
+            })
+            .then(() => {
+                getContacts()
+                    .then(contacts => {
+                        setTotal(prev => {
+                            return {
+                                ...prev,
+                                contacts: contacts
+                            }
+                        })
+                    })
+            })
+            .then(() => {
+                getScores()
+                    .then(gameScores => {
+                        setTotal(prev => {
+                            return {
+                                ...prev,
+                                gameScores: gameScores
+                            }
+                        })
+                    })
+            })
+            .then(() => setSelectedInfo(total))
+            .then(() => setSelected('total'))
+        //Falta eventos
+    }, [])
 
     const setFocus = (category) => {
         setSelected(category)
+        switch (category) {
+            case 'total':
+                setSelectedInfo(total)
+                break;
+            case 'imagenes':
+                setSelectedInfo(total.images)
+                break;
+            case 'canciones':
+                setSelectedInfo(total.songs)
+                break;
+            case 'lugares':
+                setSelectedInfo(total.locations)
+                break;
+            case 'contactos':
+                setSelectedInfo(total.contacts)
+                break;
+            case 'eventos':
+                setSelectedInfo(total.events)
+                break;
+            case 'partidas':
+                setSelectedInfo(total.gameScores)
+                break;
+        }
     }
     return (
         <div className='NudoMap StatisticsMenu'>
@@ -21,15 +105,20 @@ const StatisticsMenu = ({ setDecade }) => {
             <StatisticsFavFilter 
                 setFocus={setFocus}
             />
-            <div className='StatisticsBoxNumberDiv'>
-                <StatisticsBoxNumber 
-                    selected={selected}
-                />
-                <StatisticsBoxNumber 
-                    selected={selected}
-                    lastDays={true}
-                />
-            </div>
+            {selected ? 
+                <div className='StatisticsBoxNumberDiv'>
+                    <StatisticsBoxNumber 
+                        selected={selected}
+                        selectedInfo={selectedInfo}
+                    />
+                    <StatisticsBoxNumber 
+                        selected={selected}
+                        selectedInfo={selectedInfo}
+                        lastDays={true}
+                    />
+                </div>
+                : null
+            }
         </div>
     )
 }
