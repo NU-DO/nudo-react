a4df11c625e81df8fbbf23f1de77e5bb04455cec
miguelbachi5a <miguelbachi5a@gmail.com>
2020-11-05 20:59:06 +0100

    fix: package.json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://github.com/NU-DO/nudo-react'>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>    
