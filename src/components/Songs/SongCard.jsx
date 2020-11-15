a60954f15b76f09ffc01e65c479ebe68eb9cd8b1
miguelbachi5a <miguelbachi5a@gmail.com>
2020-11-15 10:21:34 +0100

    fix: song cards colors by decade

--- src/components/Songs/SongCard.jsx
+++ src/components/Songs/SongCard.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ButtonAddSong from './ButtonAddSong'
 import DeleteIcon from '@material-ui/icons/Delete'
 import './SongCard.scss'
 
 const SongCard = ({ song, typeSearch, form, handleOpen, addFav, handleDeleteSong }) => {
+    const [decade, setDecade] = useState('')
+
+    useEffect(() => {
+        switch (song.decade) {
+            case "50's":
+                setDecade('fifties')
+                break;
+            case "60's":
+                setDecade('sixties')
+                break;
+            case "70's":
+                setDecade('seventies')
+                break;
+            case "80's":
+                setDecade('eighties')
+                break;
+            case "90's":
+                setDecade('nineties')
+                break;
+            case "00's":
+                setDecade('current')
+                break;
+            default:
+                setDecade('')
+        }
+    }, [song])
 
     return (
-        <div className={`SongCard ${'d' + song.decade?.slice(0, 3)}`}>
+        <div className={`SongCard ${decade}`}>
             <img
                 className='albumImage'
                 src={typeSearch ? song.album.images[0].url : song.image}
--- src/components/Songs/SongFavFilter.jsx
+++ src/components/Songs/SongFavFilter.jsx
@@ -5,13 +5,13 @@ const SongFavFilter = ({ setDecade }) => {
     
     return (
         <div className='SongFavFilter'>
-            <div className='filterButton' onClick={() => setDecade('all')}>Todas</div>
-            <div className='filterButton' onClick={() => setDecade("50's")}>50's</div>
-            <div className='filterButton' onClick={() => setDecade("60's")}>60's</div>
-            <div className='filterButton' onClick={() => setDecade("70's")}>70's</div>
-            <div className='filterButton' onClick={() => setDecade("80's")}>80's</div>
-            <div className='filterButton' onClick={() => setDecade("90's")}>90's</div>
-            <div className='filterButton' onClick={() => setDecade("00's")}>00's</div>
+            <div className='filterButton allBtn' onClick={() => setDecade('all')}>Todas</div>
+            <div className='filterButton fiftiesBtn' onClick={() => setDecade("50's")}>50's</div>
+            <div className='filterButton sixtiesBtn' onClick={() => setDecade("60's")}>60's</div>
+            <div className='filterButton seventiesBtn' onClick={() => setDecade("70's")}>70's</div>
+            <div className='filterButton eightiesBtn' onClick={() => setDecade("80's")}>80's</div>
+            <div className='filterButton ninetiesBtn' onClick={() => setDecade("90's")}>90's</div>
+            <div className='filterButton currentBtn' onClick={() => setDecade("00's")}>00's</div>
         </div>
     )
 }
