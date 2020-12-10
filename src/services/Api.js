9611ebc6ad31c45db041c41702cecc494b42fb9a
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-12-10 16:03:17 +0100

    fix: change  baseURL to 3000 (was in 300)

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const http = axios.create({
   // baseURL: 'http://nudo.herokuapp.com',
   
-  baseURL: 'http://localhost:300/',
+  baseURL: 'http://localhost:3000/',
   withCredentials: true
 })
 
