ee83001d55422359845124c82cd220e958a8a669
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-16 19:09:28 +0100

    Deploy to Heroku

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://nudo.herokuapp.com',
+  baseURL: 'https://nudo.herokuapp.com',
   // baseURL: 'http://localhost:3010/',
   withCredentials: true
 })
